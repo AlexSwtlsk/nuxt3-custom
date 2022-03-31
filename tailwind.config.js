0cafd52d6968230daaea97544317324ca3814275
Alexandre Switalski <alexandre.swit@gmail.com>
2022-03-31 15:30:16 +0200

    fix(tailwind): typo in tailwind config content option

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,7 @@
 module.exports = {
   mode: 'jit',
   content: [
-    './components/**/*.{js,vue,ts}',
-    './layouts/**/*.vue',
-    './pages/**/*.vue',
-    './plugins/**/*.{js,ts}',
+    './**/*.{html, js, ts, vue}',
   ],
   theme: {
     extend: {},
