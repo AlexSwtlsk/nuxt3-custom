3d2e0ea1c83690b65209dcb772ccd5af14fd1b82
Alexandre Switalski <alexandre.swit@gmail.com>
2022-04-19 08:53:48 +0200

    feat: add hero icons autoloader component

---
+++ components/HIcon.vue
@@ -0,0 +1,24 @@
+<template>
+  <Component
+    :is="icon"
+  />
+</template>
+
+<script setup lang="ts">
+import * as solid from '@heroicons/vue/solid'
+import * as outline from '@heroicons/vue/outline'
+
+const props = defineProps({
+  name: {
+    type: String,
+    default: null,
+    validator: (value: string) => value && !value.toLowerCase().includes('icon'),
+  },
+  outline: { type: Boolean, default: false },
+})
+
+const icon = computed(() => {
+  const localIcon = (props.outline ? outline : solid)[props.name + 'Icon']
+  return localIcon ?? solid.QuestionMarkCircleIcon
+})
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,8 @@
 <template>
   <p class="text-red-500 font-bold">It works</p>
+
+  <HIcon
+    name="Bell"
+    class="h-6 w-6"
+  />
 </template>
