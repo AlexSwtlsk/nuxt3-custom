3ae46a4ae8a03b9fb1845205e646cd2530d31999
Alexandre Switalski <alexandre.swit@gmail.com>
2022-04-27 10:02:03 +0200

    chore: add one root element to page

--- pages/index.vue
+++ pages/index.vue
@@ -1,8 +1,10 @@
 <template>
-  <p class="text-red-500 font-bold">It works</p>
+  <div>
+    <p class="text-red-500 font-bold">It works</p>
 
-  <HIcon
-    name="Bell"
-    class="h-6 w-6"
-  />
+    <HIcon
+      name="Bell"
+      class="h-6 w-6"
+    />
+  </div>
 </template>
